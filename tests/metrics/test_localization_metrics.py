abc547f07faf06a9e3977ece91794918d89b42a1
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2024-05-18 15:56:56 +0200

    add model randomization test

--- tests/metrics/test_localization_metrics.py
+++ tests/metrics/test_localization_metrics.py
@@ -28,4 +28,8 @@ def test_identical_class_metrics(
     metric = IdenticalClass(device="cpu")
     score = metric(model=model, train_dataset=dataset, test_labels=test_labels, explanations=tda)["score"]
     # TODO: introduce a more meaningfull test, where the score is not zero
+    # Note from Galip:
+    # one idea could be: a random attributor should get approximately 1/( # of classes).
+    # With a big test dataset, the probability of failing a truly random test
+    # should diminish.
     assert score == expected_score
---
+++ tests/metrics/test_randomization_metrics.py
@@ -0,0 +1,22 @@
+import pytest
+import torch
+
+from metrics.randomization import MPRTMetric
+
+
+@pytest.mark.parametrize(
+    "model",
+    [
+        ("load_mnist_model"),
+    ],
+)
+def model_randomization_test(model, request):
+    model1 = request.getfixturevalue(model)
+    model2 = request.getfixturevalue(model)
+    gen = torch.Generator()
+    gen.initial_seed(42)
+    MPRTMetric._randomize_model(model2, gen)
+    for param1, param2 in zip(model1.parameters(), model2.parameters()):
+        assert (
+            torch.norm(param1.item() - param2.item()) > 1e3
+        )  # norm of the difference in parameters should be significant
