83eed8cf0aadba8a705c3f1188f69631033dbfeb
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2024-05-18 15:56:56 +0200

    add model randomization test

--- tests/metrics/test_randomization_metrics.py
+++ tests/metrics/test_randomization_metrics.py
@@ -1,6 +1,7 @@
 import pytest
 import torch
 
+<<<<<<< HEAD
 from metrics.randomization.model_randomization import ModelRandomizationMetric
 from utils.explain_wrapper import explain
 
@@ -61,3 +62,24 @@ def test_model_randomization(test_id, model, dataset, request):
     rand_model = metric.rand_model
     for param1, param2 in zip(model.parameters(), rand_model.parameters()):
         assert not torch.allclose(param1.data, param2.data), "Test failed."
+=======
+from metrics.randomization import MPRTMetric
+
+
+@pytest.mark.parametrize(
+    "model",
+    [
+        ("load_mnist_model"),
+    ],
+)
+def model_randomization_test(model, request):
+    model1 = request.getfixturevalue(model)
+    model2 = request.getfixturevalue(model)
+    gen = torch.Generator()
+    gen.initial_seed(42)
+    MPRTMetric._randomize_model(model2, gen)
+    for param1, param2 in zip(model1.parameters(), model2.parameters()):
+        assert (
+            torch.norm(param1.item() - param2.item()) > 1e3
+        )  # norm of the difference in parameters should be significant
+>>>>>>> 28150a9 (add model randomization test)
