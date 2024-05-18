b8352d8474716d7775debe5e9d8741783c4b8a15
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2024-05-18 15:35:56 +0200

    change mprt file name and location

--- src/metrics/randomization/mprt.py
+++ src/metrics/randomization/mprt.py
@@ -6,8 +6,8 @@ from metrics.base import Metric
 from utils.explanations import Explanations
 
 
-class RandomizationMetric(Metric):
-    def __init__(self, seed=42, device: str = "cpu" if torch.cuda.is_available() else "cuda"):
+class MPRTMetric(Metric):
+    def __init__(self, seed: int = 42, device: str = "cpu" if torch.cuda.is_available() else "cuda"):
         # we can move seed and device to __call__. Then we would need to set the seed per call of the metric function.
         # where does it make sense to do seeding?
         # for example, imagine the user doesn't bother giving a seed, so we use the default seed.
@@ -27,12 +27,16 @@ class RandomizationMetric(Metric):
         explain_fn_kwargs: dict,
     ):
         # Allow for precomputed random explanations?
-        randomized_model = RandomizationMetric._randomize_model(model, self.device, self.generator)
-        results = self._evaluate(explanations, randomized_model, explain_fn, explain_fn_kwargs)
+        randomized_model = MPRTMetric._randomize_model(model, self.device, self.generator)
+        rand_explanations = explain_fn(model=randomized_model, **explain_fn_kwargs)
+        rank_corr = MPRTMetric._spearman_rank_correlation(explanations, rand_explanations)
+        results = dict()
+        results["rank_correlations"] = rank_corr
+        results["average_score"] = rank_corr.mean()
         results["model_id"] = model_id
         return results
 
-    def _evaluate(
+    def _evaluate_instance(
         self,
         explanations: Explanations,
         randomized_model: torch.nn.Module,
@@ -42,15 +46,10 @@ class RandomizationMetric(Metric):
         """
         Used to implement metric-specific logic.
         """
-        rand_explanations = explain_fn(model=randomized_model, **explain_fn_kwargs)
-        rank_corr = RandomizationMetric.rank_correlation(explanations, rand_explanations)
-        results = dict()
-        results["rank_correlations"] = rank_corr
-        results["average_score"] = rank_corr.mean()
-        return results
+        pass
 
     @staticmethod
-    def _randomize_model(model, generator):
+    def _randomize_model(model: torch.nn.Module, generator: torch.Generator):
         for name, param in list(model.named_parameters()):
             random_parameter_tensor = torch.empty_like(param).normal_(generator=generator)
             names = name.split(".")
@@ -62,31 +61,29 @@ class RandomizationMetric(Metric):
         return model
 
     @staticmethod
-    def _rank_correlation(std_explanations, random_explanations):
-        # this implementation currently assumes batch sizes and number of batches are same in std and random explanations
+    def _rank_mean(explanations: Explanations):
+        train_size = explanations[0].shape[1]
+        rank_mean = torch.zeros(train_size)
+        count = 0
+        for batch in explanations:
+            _, ranks = torch.sort(batch)
+            rank_mean += torch.tensor(ranks, dtype=float) / train_size
+            count = count + batch.shape[0]
+        return rank_mean / count, count
+
+    @staticmethod
+    def _spearman_rank_correlation(std_explanations: Explanations, random_explanations: Explanations):
         train_size = std_explanations[0].shape[1]
-        std_rank_mean = torch.zeros(train_size)
-        random_rank_mean = torch.zeros(train_size)
-        std_batch_size = std_explanations.batch_size
-        random_batch_size = random_explanations.batch_size
+        std_rank_mean, test_size = MPRTMetric._rank_mean(std_explanations)
+        random_rank_mean, _ = MPRTMetric._rank_mean(random_explanations)
         corrs = torch.zeros(train_size)
         for std_batch, random_batch in zip(std_explanations, random_explanations):
             _, std_ranks = torch.sort(std_batch)
             _, random_ranks = torch.sort(random_batch)
-            std_rank_mean += torch.tensor(std_ranks, dtype=float) / train_size
-            random_rank_mean += torch.tensor(random_ranks, dtype=float) / train_size
-        std_rank_mean /= std_batch_size * len(std_explanations)
-        random_rank_mean /= random_batch_size * len(random_explanations)
-        for std_batch, random_batch in zip(std_explanations, random_explanations):
-            _, std_ranks = torch.sort(std_batch)
-            _, random_ranks = torch.sort(random_batch)
-            std_rank_mean += std_ranks / train_size
-            random_rank_mean += random_ranks / train_size
             std_ranks = std_ranks - std_rank_mean
             random_ranks = random_ranks - random_rank_mean
-            corrs = corrs + std_ranks * random_ranks
-        corrs = corrs / len(std_explanations)
-        return corrs  # return spearman rank correlation of each training data influence
+            corrs = corrs + (std_ranks * random_ranks)
+        return corrs / test_size  # return spearman rank correlation of each training data influence
 
     @staticmethod
     def _format(
