68e6e1d6a34fbeb0f769e473857c59b9320001cd
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2024-05-21 15:07:58 +0200

    add randomization reporducibility test

--- tests/metrics/test_randomization_metrics.py
+++ tests/metrics/test_randomization_metrics.py
@@ -18,3 +18,10 @@ def model_randomization_test(model, request):
     MPRTMetric._randomize_model(model2, gen)
     for param1, param2 in zip(model1.parameters(), model2.parameters()):
         assert torch.norm(param1.data - param2.data) > 1e3  # norm of the difference in parameters should be significant
+
+
+def model_randomization_test():
+    assert torch.__version__=="2.0.0"
+    gen = torch.Generator()
+    gen.manual_seed(42)
+    assert torch.all(torch.rand(5,generator=gen)==torch.Tensor([0.8823, 0.9150, 0.3829, 0.9593, 0.3904]))
