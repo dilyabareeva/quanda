f8e2ac258726b908d4a248a459ddd9f70072764c
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2024-05-22 20:06:23 +0200

    add torchmetrics to pyproject.toml to attempt to pass tests

--- tests/metrics/test_randomization_metrics.py
+++ tests/metrics/test_randomization_metrics.py
@@ -30,6 +30,7 @@ def reproducibility_test():
     gen.manual_seed(42)
     assert torch.all(torch.rand(5, generator=gen) == torch.Tensor([0.8823, 0.9150, 0.3829, 0.9593, 0.3904]))
 
+
 @pytest.mark.randomization
 @pytest.mark.parametrize(
     "model",
