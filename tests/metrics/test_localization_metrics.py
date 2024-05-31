3a2a66a061f066de5885a5de92259f11e3e2e1ab
Dilyara Bareeva <dilyabareeva@gmail.com>
2024-05-31 11:25:05 +0200

    fix identical_class.py

--- tests/metrics/test_localization_metrics.py
+++ tests/metrics/test_localization_metrics.py
@@ -25,7 +25,8 @@ def test_identical_class_metrics(
     test_labels = request.getfixturevalue(test_labels)
     dataset = request.getfixturevalue(dataset)
     tda = request.getfixturevalue(explanations)
-    metric = IdenticalClass(device="cpu")
-    score = metric(model=model, train_dataset=dataset, test_labels=test_labels, explanations=tda)["score"]
+    metric = IdenticalClass(model=model, train_dataset=dataset, device="cpu")
+    metric.update(test_labels=test_labels, explanations=tda)
+    score = metric.compute()
     # TODO: introduce a more meaningfull test, where the score is not zero
     assert score == expected_score
