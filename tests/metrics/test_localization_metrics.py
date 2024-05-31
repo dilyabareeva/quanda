2fce713965effa0948c0a64ea34fce4b5aacaadf
Dilyara Bareeva <dilyabareeva@gmail.com>
2024-05-31 11:25:05 +0200

    fix identical_class.py

--- tests/metrics/test_localization_metrics.py
+++ tests/metrics/test_localization_metrics.py
@@ -25,8 +25,9 @@ def test_identical_class_metrics(
     test_labels = request.getfixturevalue(test_labels)
     dataset = request.getfixturevalue(dataset)
     tda = request.getfixturevalue(explanations)
-    metric = IdenticalClass(device="cpu")
-    score = metric(model=model, train_dataset=dataset, test_labels=test_labels, explanations=tda)["score"]
+    metric = IdenticalClass(model=model, train_dataset=dataset, device="cpu")
+    metric.update(test_labels=test_labels, explanations=tda)
+    score = metric.compute()
     # TODO: introduce a more meaningfull test, where the score is not zero
     # Note from Galip:
     # one idea could be: a random attributor should get approximately 1/( # of classes).
