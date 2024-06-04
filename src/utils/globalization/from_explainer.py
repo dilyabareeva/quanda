37a78e73655f6d0283bce3d4f15693562a184a0d
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2024-06-04 18:01:42 +0200

    add make_func

--- src/utils/globalization/from_explainer.py
+++ src/utils/globalization/from_explainer.py
@@ -1,10 +1,12 @@
 from src.utils.globalization.base import Globalization
+from src.utils.common import make_func
 
 class GlobalizationFromSingleImageAttributor(Globalization):
     def __init__(self, training_dataset, model, attributor_fn, attributor_fn_kwargs):
         # why is it called attributor
         super().__init__(training_dataset=training_dataset)
-        self.attributor_fn = attributor_fn
+        self.attributor_fn = make_func(func=attributor_fn, func_kwargs=explain_fn_kwargs, model=self.model)
+
         self.model=model
     
     def compute_self_influences(self):
