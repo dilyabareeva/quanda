df0691e309cf9e90eea485371332df2e599a0ff8
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2024-06-04 18:11:53 +0200

    code style fixes

--- src/utils/globalization/from_explainer.py
+++ src/utils/globalization/from_explainer.py
@@ -1,18 +1,17 @@
-from src.utils.globalization.base import Globalization
 from src.utils.common import make_func
+from src.utils.globalization.base import Globalization
+
 
 class GlobalizationFromSingleImageAttributor(Globalization):
     def __init__(self, training_dataset, model, attributor_fn, attributor_fn_kwargs):
         # why is it called attributor
         super().__init__(training_dataset=training_dataset)
         self.attributor_fn = make_func(func=attributor_fn, func_kwargs=attributor_fn_kwargs, model=self.model)
+        self.model = model
 
-        self.model=model
-    
     def compute_self_influences(self):
-        for i, (x,_) in enumerate(self.training_dataset):
-            self.scores[i]=self.attributor_fn(datapoint=x)
-    
+        for i, (x, _) in enumerate(self.training_dataset):
+            self.scores[i] = self.attributor_fn(datapoint=x)
+
     def update_self_influences(self, self_influences):
-        self.scores=self_influences
-    
+        self.scores = self_influences
--- src/utils/globalization/from_explanations.py
+++ src/utils/globalization/from_explanations.py
@@ -1,5 +1,6 @@
 from src.utils.globalization.base import Globalization
 
+
 class GlobalizationFromExplanations(Globalization):
     def update(self, explanations):
         self.scores += explanations.abs().sum(dim=0)
