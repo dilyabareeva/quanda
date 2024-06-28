cc7229bb833fa2af288a1b6b671c9c50c6af2cb3
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2024-06-29 00:02:43 +0200

    Add batch size to test parameters

--- tests/explainers/test_aggregators.py
+++ tests/explainers/test_aggregators.py
@@ -6,35 +6,41 @@ from src.explainers.aggregators import AbsSumAggregator, SumAggregator
 
 @pytest.mark.aggregators
 @pytest.mark.parametrize(
-    "test_id, explanations",
+    "test_id, explanations, batch_size",
     [
         (
             "mnist",
             "load_mnist_explanations_1",
+            1,
         ),
     ],
 )
-def test_sum_aggregator(test_id, explanations, request):
+def test_sum_aggregator(test_id, explanations, batch_size, request):
     explanations = request.getfixturevalue(explanations)
     aggregator = SumAggregator()
-    aggregator.update(explanations)
+    for i in range(0, explanations.shape[0], batch_size):
+        upper_index = min(explanations.shape[0], i + batch_size)
+        aggregator.update(explanations[i:upper_index])
     global_rank = aggregator.compute().argsort()
     assert torch.allclose(global_rank, explanations.sum(dim=0).argsort())
 
 
 @pytest.mark.aggregators
 @pytest.mark.parametrize(
-    "test_id, explanations",
+    "test_id, explanations, batch_size",
     [
         (
             "mnist",
             "load_mnist_explanations_1",
+            1,
         ),
     ],
 )
-def test_abs_aggregator(test_id, explanations, request):
+def test_abs_aggregator(test_id, explanations, batch_size, request):
     explanations = request.getfixturevalue(explanations)
     aggregator = AbsSumAggregator()
-    aggregator.update(explanations)
+    for i in range(0, explanations.shape[0], batch_size):
+        upper_index = min(explanations.shape[0], i + batch_size)
+        aggregator.update(explanations[i:upper_index])
     global_rank = aggregator.compute().argsort()
     assert torch.allclose(global_rank, explanations.abs().mean(dim=0).argsort())
