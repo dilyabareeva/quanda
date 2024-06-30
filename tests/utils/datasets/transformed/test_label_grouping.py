7dba3f4b51ca37fa6246dd4cd6c7e1859bcfb45b
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2024-06-30 02:41:23 +0200

    minor fix

--- tests/utils/datasets/transformed/test_label_grouping.py
+++ tests/utils/datasets/transformed/test_label_grouping.py
@@ -41,6 +41,6 @@ def test_label_grouping(
     for i in range(len(grouped_dataset)):
         x, g = grouped_dataset[i]
         y = grouped_dataset._get_original_label(i)
-        assertions.append((i not in grouped_dataset.samples_to_perturb) or (g == grouped_dataset.class_to_group[y]))
+        assertions.append((i not in grouped_dataset.transform_indices) or (g == grouped_dataset.class_to_group[y]))
 
     assert all(assertions)
