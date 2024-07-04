866e195cff042fa91b4588976e71945f1e5023d5
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2024-07-04 15:27:21 +0200

    add missing import

--- src/explainers/wrappers/captum_influence.py
+++ src/explainers/wrappers/captum_influence.py
@@ -1,6 +1,6 @@
 import warnings
 from inspect import signature
-from typing import Any, Callable, List, Optional, Union
+from typing import Any, Callable, List, Optional, Union, Sized
 
 import torch
 from captum.influence import SimilarityInfluence  # type: ignore
@@ -61,8 +61,12 @@ class CaptumInfluence(BaseExplainer):
         sig = signature(self.captum_explainer.influence).parameters.keys()
         # TODO:HANDLE CASES WHERE WE MIGHT WANT TO PASS EXTRA PARAMETERS.
         # THESE SHOULD BE TAKEN IN __init__, NOT AS EXTRA PARAMETERS TO THE .explain CALL.
-        dataset_size =len(self.dataset) if isinstance(self.dataset, Sized) else len(torch.utils.data.DataLoader(self.dataset, batch_size=1))
-            
+        dataset_size = (
+            len(self.dataset)
+            if isinstance(self.dataset, Sized)
+            else len(torch.utils.data.DataLoader(self.dataset, batch_size=1))
+        )
+
         if "top_k" in sig:
             extra_kwargs["top_k"] = dataset_size
 
