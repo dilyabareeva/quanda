2d75537135df3b433ec7ae4403531ccef05a14b8
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2024-07-18 18:08:31 +0200

    add test_common.py

---
+++ tests/utils/test_common.py
@@ -0,0 +1,21 @@
+import pytest
+
+from src.utils.common import make_func
+
+
+@pytest.mark.utils
+@pytest.mark.parametrize(
+    "test_id",
+    [
+        ("make_func",),
+    ],
+)
+def test_trainer(
+    test_id,
+    request,
+):
+    def foo(x, y, z):
+        return x * y + z
+
+    bar = make_func(foo, {"x": 100, "z": 1})
+    assert bar(y=1) == 101
