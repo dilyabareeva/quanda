e80a9ac1ec2da39517498dca51912a75447f5ad2
dilyabareeva <dilyabareeva@gmail.com>
2024-08-02 13:23:32 +0200

    mypy error fix

--- tests/metrics/test_unnamed_metrics.py
+++ tests/metrics/test_unnamed_metrics.py
@@ -4,7 +4,6 @@ from src.explainers.wrappers.captum_influence import CaptumSimilarity
 from src.metrics.unnamed.dataset_cleaning import DatasetCleaningMetric
 from src.metrics.unnamed.top_k_overlap import TopKOverlapMetric
 from src.utils.functions.similarities import cosine_similarity
-from src.utils.training.base_pl_module import BasicLightningModule
 from src.utils.training.trainer import Trainer
 
 
