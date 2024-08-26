b0017afdb94087678cdb2d06dc97f8a9417dfda7
dilyabareeva <dilyabareeva@gmail.com>
2024-08-26 10:40:24 +0200

    model-to-debug draft

--- tutorials/tiny_image_net_resnet18_train.py
+++ tutorials/tiny_image_net_resnet18_train.py
@@ -17,8 +17,11 @@ from torch.optim import lr_scheduler
 from torchmetrics.functional import accuracy
 import glob
 from torchvision.io import read_image, ImageReadMode
-from tutorials.tiny_imagenet_dataset import TrainTinyImageNetDataset, HoldOutTinyImageNetDataset
+from tutorials.tiny_imagenet_dataset import TrainTinyImageNetDataset, HoldOutTinyImageNetDataset, SingleClassVisionDataset
+import nltk
+from nltk.corpus import wordnet as wn
 
+from quanda.utils.datasets.transformed import LabelGroupingDataset, LabelFlippingDataset, SampleTransformationDataset
 
 os.environ['NCCL_P2P_DISABLE'] = "1"
 os.environ['NCCL_IB_DISABLE'] = "1"
@@ -31,20 +34,123 @@ n_classes = 200
 batch_size = 64
 num_workers = 8
 local_path = "/home/bareeva/Projects/data_attribution_evaluation/assets/tiny-imagenet-200"
+goldfish_sketch_path = "/data1/datapool/sketch/n01443537"
 rng = torch.Generator().manual_seed(42)
 
-
-transforms = transforms.Compose(
+regular_transforms = transforms.Compose(
         [
             transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
         ]
     )
+backdoor_transforms = transforms.Compose(
+        [
+            transforms.Resize((64, 64)),
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+        ]
+    )
 
-train_set = TrainTinyImageNetDataset(local_path=local_path, transforms=transforms)
+def is_target_in_parents_path(synset, target_synset):
+    """
+    Given a synset, return True if the target_synset is in its parent path, else False.
+    """
+    # Check if the current synset is the target synset
+    if synset == target_synset:
+        return True
+
+    # Recursively check all parent synsets
+    for parent in synset.hypernyms():
+        if is_target_in_parents_path(parent, target_synset):
+            return True
+
+    # If target_synset is not found in any parent path
+    return False
+
+def get_all_descendants(target):
+    objects = set()
+    target_synset = wn.synsets(target, pos=wn.NOUN)[0]  # Get the target synset
+    with open(local_path + '/wnids.txt', 'r') as f:
+        for line in f:
+            synset = wn.synset_from_pos_and_offset('n', int(line.strip()[1:]))
+            if is_target_in_parents_path(synset, target_synset):
+                objects.add(line.strip())
+    return objects
+
+# dogs
+dogs = get_all_descendants('dog')
+cats = get_all_descendants('cat')
+
+
+id_dict = {}
+with open(local_path + '/wnids.txt', 'r') as f:
+    id_dict = {line.strip(): i for i, line in enumerate(f)}
+
+
+class_to_group = {id_dict[k]: i for i, k in enumerate(id_dict) if k not in dogs.union(cats)}
+n_classes = len(class_to_group) + 2
+class_to_group.update({id_dict[k]: len(class_to_group) for k in dogs})
+class_to_group.update({id_dict[k]: len(class_to_group) for k in cats})
+
+
+# function to add a yellow square to an image in torchvision
+def add_yellow_square(img):
+    img[0, 10:13, 10:13] = 1
+    img[1, 10:13, 10:13] = 1
+    img[2, 10:13, 10:13] = 0
+    return img
+
+
+# backdoor dataset that combines two dataset and adds 100 backdoor samples from dataset 2 to class 0 of dataset 1
+def backdoored_dataset(dataset1, backdoor_samples, backdoor_label):
+    for i in range(len(backdoor_samples)):
+        backdoor_samples[i] = (backdoor_samples[i][0], backdoor_label)
+    dataset1 = torch.utils.data.ConcatDataset([backdoor_samples, dataset1])
+    return dataset1
+
+
+def flipped_group_dataset(train_set, n_classes, regular_transforms, seed, class_to_group, shortcut_fn, p_shortcut,
+                          p_flipping, backdoor_dataset, backdoor_label):
+    group_dataset = LabelGroupingDataset(
+        dataset=train_set,
+        n_classes=n_classes,
+        dataset_transform=regular_transforms,
+        class_to_group=class_to_group,
+        seed=seed,
+    )
+    flipped = LabelFlippingDataset(
+        dataset=group_dataset,
+        n_classes=n_classes,
+        dataset_transform=regular_transforms,
+        p=p_flipping,
+        seed=seed,
+    )
+
+    sc_dataset = SampleTransformationDataset(
+        dataset=flipped,
+        n_classes=n_classes,
+        dataset_transform=regular_transforms,
+        cls_idx=None,
+        p=p_shortcut,
+        seed=seed,
+        sample_fn=shortcut_fn,
+    )
+
+    return backdoored_dataset(sc_dataset, backdoor_dataset, backdoor_label)
+
+
+train_set = TrainTinyImageNetDataset(local_path=local_path, transforms=regular_transforms)
+goldfish_dataset = SingleClassVisionDataset(path=goldfish_sketch_path, transforms=backdoor_transforms)
+# split goldfish dataset into train (100) and val (100)
+goldfish_set, _ = torch.utils.data.random_split(goldfish_dataset, [200, len(goldfish_dataset)-200], generator=rng)
+
+
+train_set = flipped_group_dataset(train_set, n_classes, regular_transforms, seed=42,
+                                  class_to_group=class_to_group, shortcut_fn=add_yellow_square,
+                                  p_shortcut=0.1, p_flipping=0.1, backdoor_dataset=goldfish_set,
+                                  backdoor_label=1)
+train_set, val_set = torch.utils.data.random_split(train_set, [0.95, 0.05], generator=rng)
 train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=num_workers)
 
-hold_out = HoldOutTinyImageNetDataset(local_path=local_path, transforms=transforms)
-test_set, val_set = torch.utils.data.random_split(hold_out, [0.5, 0.5], generator=rng)
+test_set = HoldOutTinyImageNetDataset(local_path=local_path, transforms=regular_transforms)
 test_dataloader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 val_dataloader = torch.utils.data.DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
