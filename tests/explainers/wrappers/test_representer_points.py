6b2300e3f9b290149b54dc89420d651f03e601a8
dilyabareeva <dilyabareeva@gmail.com>
2024-09-09 00:08:28 +0200

    add representer points wrapper and test

---
+++ tests/explainers/wrappers/test_representer_points.py
@@ -0,0 +1,33 @@
+import os
+
+import pytest
+import torch
+
+from quanda.explainers.wrappers import RepresenterPoints
+
+
+@pytest.mark.explainers
+@pytest.mark.parametrize(
+    "test_id, model, dataset, test_tensor, test_labels, method_kwargs",
+    [
+        (
+            "mnist_representer",
+            "load_mnist_model",
+            "load_mnist_dataset",
+            "load_mnist_test_samples_1",
+            "load_mnist_test_labels_1",
+            {"model_id": "0", "batch_size": 8, "features_layer": "relu_4", "classifier_layer": "fc_3"},
+        ),
+    ],
+)
+def test_representer_points(test_id, model, dataset, test_tensor, test_labels, method_kwargs, request, tmp_path):
+    model = request.getfixturevalue(model)
+    dataset = request.getfixturevalue(dataset)
+    test_tensor = request.getfixturevalue(test_tensor)
+    test_labels = request.getfixturevalue(test_labels)
+
+    explainer = RepresenterPoints(model=model, cache_dir=str(tmp_path), train_dataset=dataset, **method_kwargs)
+
+    explanations = explainer.explain(test=test_tensor, targets=test_labels)
+
+    assert explanations.shape[0] == len(test_labels), "Explanations shape is not as expected"
