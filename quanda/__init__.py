1c398fb69a802c0fcbd86c4c36a5a99455a08b41
dilyabareeva <dilyabareeva@gmail.com>
2024-09-15 12:45:54 +0200

    add logging

--- quanda/__init__.py
+++ quanda/__init__.py
@@ -1,3 +1,11 @@
 from quanda import benchmarks, explainers, metrics, utils
 
 __all__ = ["explainers", "metrics", "benchmarks", "utils"]
+
+
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
