3d835db337d32ebf04fda153592b2ee661aec66b
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2024-09-27 22:31:46 +0200

    downstream eval download tests

--- tests/benchmarks/downstream_eval/test_shortcut_detection.py
+++ tests/benchmarks/downstream_eval/test_shortcut_detection.py
@@ -1,11 +1,14 @@
 import math
+from functools import reduce
 
 import lightning as L
 import pytest
 import torch
+from torcheval.metrics.functional import binary_auprc
 
 from quanda.benchmarks.downstream_eval import ShortcutDetection
 from quanda.explainers.wrappers.captum_influence import CaptumSimilarity
+from quanda.utils.datasets.transformed import SampleTransformationDataset
 from quanda.utils.functions.similarities import cosine_similarity
 from quanda.utils.training.trainer import Trainer
 
@@ -214,11 +217,11 @@ def test_shortcut_detection_generate_from_pl_module(
 
 @pytest.mark.benchmarks
 @pytest.mark.parametrize(
-    "test_id, benchmark_name, batch_size, explainer_cls, expl_kwargs, expected_score",
+    "test_id, benchmark, batch_size, explainer_cls, expl_kwargs, filter_by_class, filter_by_prediction, expected_score",
     [
         (
             "mnist",
-            "mnist_shortcut_detection",
+            "mnist_shortcut_detection_benchmark",
             8,
             CaptumSimilarity,
             {
@@ -226,35 +229,142 @@ def test_shortcut_detection_generate_from_pl_module(
                 "similarity_metric": cosine_similarity,
                 "load_from_disk": True,
             },
-            0.875,
+            True,
+            True,
+            "compute",
+        ),
+        (
+            "mnist",
+            "mnist_shortcut_detection_benchmark",
+            8,
+            CaptumSimilarity,
+            {
+                "layers": "model.fc_2",
+                "similarity_metric": cosine_similarity,
+                "load_from_disk": True,
+            },
+            True,
+            False,
+            "compute",
+        ),
+        (
+            "mnist",
+            "mnist_shortcut_detection_benchmark",
+            8,
+            CaptumSimilarity,
+            {
+                "layers": "model.fc_2",
+                "similarity_metric": cosine_similarity,
+                "load_from_disk": True,
+            },
+            False,
+            True,
+            "compute",
+        ),
+        (
+            "mnist",
+            "mnist_shortcut_detection_benchmark",
+            8,
+            CaptumSimilarity,
+            {
+                "layers": "model.fc_2",
+                "similarity_metric": cosine_similarity,
+                "load_from_disk": True,
+            },
+            False,
+            False,
+            "compute",
         ),
     ],
 )
 def test_shortcut_detection_download(
     test_id,
-    benchmark_name,
+    benchmark,
     batch_size,
     explainer_cls,
     expl_kwargs,
+    filter_by_class,
+    filter_by_prediction,
     expected_score,
     tmp_path,
+    request,
 ):
-    dst_eval = ShortcutDetection.download(
-        name=benchmark_name,
-        cache_dir=str(tmp_path),
-        device="cpu",
-    )
+    dst_eval = request.getfixturevalue(benchmark)
 
     expl_kwargs = {"model_id": "0", "cache_dir": str(tmp_path), **expl_kwargs}
     dst_eval.train_dataset = torch.utils.data.Subset(dst_eval.train_dataset, list(range(16)))
     dst_eval.shortcut_dataset = torch.utils.data.Subset(dst_eval.shortcut_dataset, list(range(16)))
     dst_eval.eval_dataset = torch.utils.data.Subset(dst_eval.eval_dataset, list(range(16)))
     dst_eval.shortcut_indices = [i for i in dst_eval.shortcut_indices if i < 16]
+    dst_eval.filter_by_class = filter_by_class
+    dst_eval.filter_by_prediction = filter_by_prediction
 
     score = dst_eval.evaluate(
         explainer_cls=explainer_cls,
         expl_kwargs=expl_kwargs,
         batch_size=batch_size,
     )["score"]
+    if expected_score == "compute":
+        activation = []
+
+        def hook(model, input, output):
+            activation.append(output.detach())
+
+        exp_layer = reduce(getattr, expl_kwargs["layers"].split("."), dst_eval.model)
+        exp_layer.register_forward_hook(hook)
+        shortcut_expl_ds = SampleTransformationDataset(
+            dataset=dst_eval.eval_dataset,
+            dataset_transform=dst_eval.dataset_transform,
+            n_classes=dst_eval.n_classes,
+            sample_fn=dst_eval.sample_fn,
+            p=1.0,
+        )
+        train_ld = torch.utils.data.DataLoader(dst_eval.train_dataset, batch_size=16, shuffle=False)
+        test_ld = torch.utils.data.DataLoader(shortcut_expl_ds, batch_size=16, shuffle=False)
+        for x, y in iter(train_ld):
+            x = x.to(dst_eval.device)
+            y_train = y.to(dst_eval.device)
+            dst_eval.model(x)
+        act_train = activation[0]
+        activation = []
+        for x, y in iter(test_ld):
+            x = x.to(dst_eval.device)
+            y_test = y.to(dst_eval.device)
+            y_preds = dst_eval.model(x).argmax(dim=-1)
+            select_idx = torch.tensor([True] * 16)
+            if filter_by_class:
+                select_idx *= y_test != dst_eval.shortcut_cls
+            if filter_by_prediction:
+                select_idx *= y_preds == dst_eval.shortcut_cls
+            dst_eval.model(x)
+        act_test = activation[0]
+        act_test = act_test[select_idx]
+        act_test = torch.nn.functional.normalize(act_test, dim=-1)
+        act_train = torch.nn.functional.normalize(act_train, dim=-1)
+        IP = torch.matmul(act_test, act_train.T)
+        binary_shortcut_indices: torch.Tensor = torch.tensor(
+            [1 if i in dst_eval.shortcut_indices else 0 for i in range(16)], device=dst_eval.device
+        )
+        expected_score = torch.tensor([binary_auprc(xpl, binary_shortcut_indices) for xpl in IP]).mean().item()
 
     assert math.isclose(score, expected_score, abs_tol=0.00001)
+
+
+@pytest.mark.benchmarks
+@pytest.mark.parametrize(
+    "test_id, benchmark",
+    [
+        (
+            "mnist",
+            "mnist_shortcut_detection_benchmark",
+        ),
+    ],
+)
+def test_shortcut_detection_download_sanity_checks(test_id, benchmark, request):
+    dst_eval = request.getfixturevalue(benchmark)
+    assertions = []
+    for i in dst_eval.shortcut_indices:
+        x, y = dst_eval.shortcut_dataset[i]
+        assertions.append(y == dst_eval.shortcut_cls)
+        assertions.append(torch.allclose(x[0, 15:23, 15:23], torch.ones(8, 8)))
+    assert all(assertions)
--- tests/benchmarks/downstream_eval/test_subclass_detection.py
+++ tests/benchmarks/downstream_eval/test_subclass_detection.py
@@ -1,4 +1,5 @@
 import math
+from functools import reduce
 
 import lightning as L
 import pytest
@@ -209,11 +210,11 @@ def test_subclass_detection_generate_lightning_model(
 
 @pytest.mark.benchmarks
 @pytest.mark.parametrize(
-    "test_id, benchmark_name, batch_size, explainer_cls, expl_kwargs, expected_score",
+    "test_id, benchmark, batch_size, explainer_cls, expl_kwargs, expected_score",
     [
         (
             "mnist",
-            "mnist_subclass_detection",
+            "mnist_subclass_detection_benchmark",
             8,
             CaptumSimilarity,
             {
@@ -223,24 +224,27 @@ def test_subclass_detection_generate_lightning_model(
             },
             0.6875,
         ),
+        (
+            "mnist",
+            "mnist_subclass_detection_benchmark",
+            8,
+            CaptumSimilarity,
+            {
+                "layers": "model.fc_2",
+                "similarity_metric": cosine_similarity,
+                "load_from_disk": True,
+            },
+            "compute",
+        ),
     ],
 )
 def test_subclass_detection_download(
-    test_id,
-    benchmark_name,
-    batch_size,
-    explainer_cls,
-    expl_kwargs,
-    expected_score,
-    tmp_path,
+    test_id, benchmark, batch_size, explainer_cls, expl_kwargs, expected_score, tmp_path, request
 ):
-    dst_eval = SubclassDetection.download(
-        name=benchmark_name,
-        cache_dir=str(tmp_path),
-        device="cpu",
-    )
+    dst_eval = request.getfixturevalue(benchmark)
 
     expl_kwargs = {"model_id": "0", "cache_dir": str(tmp_path), **expl_kwargs}
+    dst_eval.train_dataset = torch.utils.data.Subset(dst_eval.train_dataset, list(range(16)))
     dst_eval.grouped_dataset = torch.utils.data.Subset(dst_eval.grouped_dataset, list(range(16)))
     dst_eval.train_dataset = torch.utils.data.Subset(dst_eval.train_dataset, list(range(16)))
     dst_eval.eval_dataset = torch.utils.data.Subset(dst_eval.eval_dataset, list(range(16)))
@@ -249,5 +253,53 @@ def test_subclass_detection_download(
         expl_kwargs=expl_kwargs,
         batch_size=batch_size,
     )["score"]
+    if expected_score == "compute":
+        activation = []
+
+        def hook(model, input, output):
+            activation.append(output.detach())
+
+        exp_layer = reduce(getattr, expl_kwargs["layers"].split("."), dst_eval.group_model)
+        exp_layer.register_forward_hook(hook)
+        train_ld = torch.utils.data.DataLoader(dst_eval.grouped_dataset, batch_size=16, shuffle=False)
+        test_ld = torch.utils.data.DataLoader(dst_eval.eval_dataset, batch_size=16, shuffle=False)
+        for x, y in iter(train_ld):
+            x = x.to(dst_eval.device)
+            dst_eval.group_model(x)
+        act_train = activation[0]
+        activation = []
+        y_train = torch.tensor([y for x, y in dst_eval.train_dataset])
+        for x, y in iter(test_ld):
+            x = x.to(dst_eval.device)
+            y_test = y.to(dst_eval.device)
+            y_preds = dst_eval.group_model(x).argmax(dim=-1)
+            dst_eval.group_model(x)
+        act_test = activation[0]
+        act_test = torch.nn.functional.normalize(act_test, dim=-1)
+        act_train = torch.nn.functional.normalize(act_train, dim=-1)
+        IP = torch.matmul(act_test, act_train.T)
+        max_attr_indices = IP.argmax(dim=-1)
+        expected_score = torch.sum((y_train[max_attr_indices] == y_test) * 1.0) / act_test.shape[0]
 
     assert math.isclose(score, expected_score, abs_tol=0.00001)
+
+
+@pytest.mark.benchmark
+@pytest.mark.parametrize(
+    "test_id, benchmark, batch_size",
+    [
+        (
+            "mnist",
+            "mnist_subclass_detection_benchmark",
+            8,
+        ),
+    ],
+)
+def test_subclass_detection_download_sanity_checks(test_id, benchmark, batch_size, request):
+    dst_eval = request.getfixturevalue(benchmark)
+    ldr = torch.utils.data.DataLoader(dst_eval.grouped_dataset, batch_size=batch_size)
+    assertions = [
+        dst_eval.grouped_dataset[i][1] == dst_eval.class_to_group[dst_eval.train_dataset[i][1]]
+        for i in range(len(dst_eval.grouped_dataset))
+    ]
+    assert all(assertions)
