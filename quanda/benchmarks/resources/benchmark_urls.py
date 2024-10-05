905b1c44bc05ecc82f99542a7ee8ce36b98f5207
dilyabareeva <dilyabareeva@gmail.com>
2024-10-05 14:29:50 +0200

    remove tiny url

--- quanda/benchmarks/resources/benchmark_urls.py
+++ quanda/benchmarks/resources/benchmark_urls.py
@@ -1,8 +1,8 @@
 benchmark_urls: dict = {
-    "mnist_top_k_cardinality": "https://tinyurl.com/4a5zh7ha",
-    "mnist_subclass_detection": "https://tinyurl.com/23ms47p4",
-    "mnist_mixed_datasets": "https://tinyurl.com/rcwj92ez",
-    "mnist_mislabeling_detection": "https://tinyurl.com/5c69xyh7",
-    "mnist_shortcut_detection": "https://tinyurl.com/bdt8nr78",
-    "mnist_class_detection": "https://tinyurl.com/bddcx96y",
+    "mnist_top_k_cardinality": "https://datacloud.hhi.fraunhofer.de/s/RNLBjCToBT9o2Zd/download/mnist_top_k_overlap.pth",
+    "mnist_subclass_detection": "https://datacloud.hhi.fraunhofer.de/s/HygEsYPH3wqEGmX/download/mnist_subclass_detection.pth",
+    "mnist_mixed_datasets": "https://datacloud.hhi.fraunhofer.de/s/BNpJpL5YJ8L5yGg/download/mnist_mixed_datasets.pth",
+    "mnist_mislabeling_detection": "https://datacloud.hhi.fraunhofer.de/s/qzN9Wr8Liq3i2SY/download/mnist_mislabeling_detection.pth",
+    "mnist_shortcut_detection": "https://datacloud.hhi.fraunhofer.de/s/yYGpZrRpDdbgXyT/download/mnist_shortcut_detection.pth",
+    "mnist_class_detection": "https://datacloud.hhi.fraunhofer.de/s/R3XoDDHq4eNqjp3/download/mnist_class_detection.pth",
 }
