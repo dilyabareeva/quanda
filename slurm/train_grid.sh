828d210e36509e1a6866ed1541946df816cdc136
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2024-11-19 01:55:39 +0100

    minor change

--- slurm/train_grid.sh
+++ slurm/train_grid.sh
@@ -6,8 +6,8 @@ do
     do
       for weight_decay in 0.0 0.01 0.001
       do
-        sbatch train.sh --dataset_type $1 --epochs 100 --save_each 10 --batch_size 32 --device cuda --optimizer $opt --lr $lr --scheduler $step --weight_decay $weight_decay
-        sbatch train.sh --dataset_type $1 --epochs 100 --save_each 10 --batch_size 32 --device cuda --optimizer $opt --lr $lr --scheduler $step --weight_decay $weight_decay --pretrained
+        sbatch train.sh --dataset_type $1 --epochs 100 --save_each 10 --batch_size 32 --device cuda --optimizer $opt --lr $lr --scheduler $scheduler --weight_decay $weight_decay
+        sbatch train.sh --dataset_type $1 --epochs 100 --save_each 10 --batch_size 32 --device cuda --optimizer $opt --lr $lr --scheduler $scheduler --weight_decay $weight_decay --pretrained
       done
     done
   done
