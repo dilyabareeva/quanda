1faecad89dd277c33565454ff55f3c6efc219d54
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2024-11-25 16:06:01 +0100

    feat: added class_detection, top_k_cardinality, mislabeling_detection and shortcut_detection benchmarks for tiny imagenet

--- quanda/benchmarks/resources/benchmark_urls.py
+++ quanda/benchmarks/resources/benchmark_urls.py
@@ -5,4 +5,8 @@ benchmark_urls: dict = {
     "mnist_mislabeling_detection": "https://datacloud.hhi.fraunhofer.de/s/qzN9Wr8Liq3i2SY/download/mnist_mislabeling_detection.pth",
     "mnist_shortcut_detection": "https://datacloud.hhi.fraunhofer.de/s/yYGpZrRpDdbgXyT/download/mnist_shortcut_detection.pth",
     "mnist_class_detection": "https://datacloud.hhi.fraunhofer.de/s/R3XoDDHq4eNqjp3/download/mnist_class_detection.pth",
+    "tiny_imagenet_class_detection": "https://datacloud.hhi.fraunhofer.de/s/4Ma697fEyQYeyoR/tiny_imagenet_class_detection.pth",
+    "tiny_imagenet_top_k_cardinality": "https://datacloud.hhi.fraunhofer.de/s/SRYDnf4LbMAZ222/download/tiny_imagenet_top_k_cardinality.pth",
+    "tiny_imagenet_mislabeling_detection": "https://datacloud.hhi.fraunhofer.de/s/wqNYz8gCKks3RBn/download/tiny_imagenet_mislabeling_detection.pth",
+    "tiny_imagenet_shortcut_detection": "https://datacloud.hhi.fraunhofer.de/s/yDTMsyyLYK8sTBc/download/tiny_imagenet_shortcut_detection.pth",
 }
--- quanda/benchmarks/resources/sample_transforms.py
+++ quanda/benchmarks/resources/sample_transforms.py
@@ -9,6 +9,13 @@ def add_white_square_mnist(img):
     return img
 
 
+def add_yellow_square(img):
+    square_size = (15, 15)  # Size of the square
+    yellow_square = Image.new("RGB", square_size, (255, 255, 0))  # Create a yellow square
+    img.paste(yellow_square, (10, 10))  # Paste it onto the image at the specified position
+    return img
+
+
 sample_transforms = {
     "mnist_transforms": transforms.Compose(
         [transforms.Resize((28, 28)), transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
@@ -20,4 +27,15 @@ sample_transforms = {
         [transforms.Normalize(mean=[0], std=[1 / 0.5])] + [transforms.Normalize(mean=[0.5], std=[1])]
     ),
     "add_white_square_mnist": add_white_square_mnist,
+    "tiny_imagenet_transforms": transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))]
+    ),
+    "tiny_imagener_adversarial_transforms": transforms.Compose(
+        [
+            transforms.Resize(size=(64, 64)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        ]
+    ),
+    "add_yellow_square": add_yellow_square,
 }
