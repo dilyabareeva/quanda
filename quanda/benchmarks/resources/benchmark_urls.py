1386da22f4be0e8303e798f0c309df4756f4c975
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2024-12-06 12:47:11 +0100

    fix: remove faulty benchmark urls

--- quanda/benchmarks/resources/benchmark_urls.py
+++ quanda/benchmarks/resources/benchmark_urls.py
@@ -7,8 +7,4 @@ benchmark_urls: dict = {
     "mnist_mislabeling_detection": "https://datacloud.hhi.fraunhofer.de/s/nwfP4ojrSfHq69e/download/mnist_shortcut_detection.pth",
     "mnist_shortcut_detection": "https://datacloud.hhi.fraunhofer.de/s/CLYLTbb4Zb74tHA/download/mnist_mislabeling_detection.pth",
     "mnist_class_detection": "https://datacloud.hhi.fraunhofer.de/s/r68RfiGnQSrd3RT/download/mnist_class_detection.pth",
-    "tiny_imagenet_class_detection": "https://datacloud.hhi.fraunhofer.de/s/P5Hr7cBPdjSWsyX/download/tiny_imagenet_class_detection.pth",
-    "tiny_imagenet_mislabeling_detection": "https://datacloud.hhi.fraunhofer.de/s/HwiCwE62MjfJBpG/download/tiny_imagenet_mislabeling_detection.pth",
-    "tiny_imagenet_shortcut_detection": "https://datacloud.hhi.fraunhofer.de/s/naik8grLtXbnT92/download/tiny_imagenet_shortcut_detection.pth",
-    "tiny_imagenet_top_k_cardinality": "https://datacloud.hhi.fraunhofer.de/s/iEJA9jYFaXmy6RN/download/tiny_imagenet_top_k_cardinality.pth",
 }
