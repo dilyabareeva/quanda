0966b01ae5701f082d1b2b3dd0378754a67706ba
dilyabareeva <dilyabareeva@gmail.com>
2025-02-18 20:36:30 +0100

    feat: hydra optuna sweeper for hyperparameter search

--- bench_prep/train.py
+++ bench_prep/train.py
@@ -1,8 +1,6 @@
 import hydra
 from omegaconf import DictConfig, OmegaConf
 import os
-# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 
 from quanda.benchmarks import bench_dict
 from quanda.benchmarks.downstream_eval import *
@@ -12,18 +10,16 @@ from quanda.benchmarks.config_parser import BenchConfigParser
 
 
 @hydra.main(version_base=None, config_path="../config", config_name="default")
-def main(cfg: DictConfig) -> None:
+def main(cfg: DictConfig) -> float:
     cfg.cfg_file_name = f"{cfg.cfg_file_name}_{cfg.bench}.yaml"
+
     bench_cls = bench_dict[cfg.bench]
     logger = BenchConfigParser.parse_logger(cfg)
-    bench = bench_cls.train(cfg, logger=logger)
+    bench = bench_cls.train(cfg)
     scores = bench.sanity_check()
-    logger.log(scores)
-    print(bench.name)
-    # Save config to the specified output directory
-    output_file = os.path.join(cfg.cfg_output_dir, cfg.cfg_file_name)
-    with open(output_file, "w") as file:
-        OmegaConf.save(cfg, file)
+    logger.log_metrics(scores)
+
+    return list(scores.values())[0]
 
 
 if __name__ == "__main__":
--- bench_prep/train_mnist.sh
+++ bench_prep/train_mnist.sh
@@ -25,7 +25,7 @@ for params in "${param_dicts[@]}"; do
     # Construct and execute the command with Hydra overrides
     echo "Running with parameters: $params"
     echo "Saving output to: $cfg_output_dir/$cfg_file_name"
-    python bench_prep/train.py $params hydra.run.dir="hydra_logs" id=$id +cfg_file_name=$cfg_file_name +cfg_output_dir=$cfg_output_dir
+    python bench_prep/train.py $params id=$id +cfg_file_name=$cfg_file_name +cfg_output_dir=$cfg_output_dir --multirun
     echo "Finished running with parameters: $params"
     echo "--------------------------------------"
 done 
