b6912b6d5e6a081f2fda825763fc8e305d3ba54a
dilyabareeva <dilyabareeva@gmail.com>
2025-07-15 11:48:11 +0200

    fix: package resource for benchmarks

--- quanda/benchmarks/resources/config_map.py
+++ quanda/benchmarks/resources/config_map.py
@@ -1,5 +1,8 @@
 """Here benchmark handles will be match to the config files."""
 
+from importlib.resources import files
+
+
 config_map: dict = {
     "mnist_top_k_cardinality_unit": "tests/assets/mnist_test_suite_3/"
     "0921427-default_ClassDetection.yaml",
@@ -15,18 +18,18 @@ config_map: dict = {
     "0921427-default_ClassDetection.yaml",
     "mnist_model_randomization_unit": "tests/assets/mnist_test_suite_3/"
     "0921427-default_ClassDetection.yaml",
-    "mnist_top_k_cardinality": "quanda/benchmarks/resources/configs/"
-    "7b6c898-default_ClassDetection.yaml",
-    "mnist_subclass_detection": "quanda/benchmarks/resources/configs/"
-    "7b6c898-default_SubclassDetection.yaml",
-    "mnist_mixed_datasets": "quanda/benchmarks/resources/configs/"
-    "7b6c898-default_MixedDatasets.yaml",
-    "mnist_mislabeling_detection": "quanda/benchmarks/resources/configs/"
-    "7b6c898-default_MislabelingDetection.yaml",
-    "mnist_shortcut_detection": "quanda/benchmarks/resources/configs/"
-    "7b6c898-default_ShortcutDetection.yaml",
-    "mnist_class_detection": "quanda/benchmarks/resources/configs/"
-    "7b6c898-default_ClassDetection.yaml",
-    "mnist_model_randomization": "quanda/benchmarks/resources/configs/"
-    "7b6c898-default_ClassDetection.yaml",
+    "mnist_top_k_cardinality": files("quanda.benchmarks.resources.configs").joinpath(
+    "7b6c898-default_ClassDetection.yaml"),
+    "mnist_subclass_detection": files("quanda.benchmarks.resources.configs").joinpath(
+    "7b6c898-default_SubclassDetection.yaml"),
+    "mnist_mixed_datasets": files("quanda.benchmarks.resources.configs").joinpath(
+    "7b6c898-default_MixedDatasets.yaml"),
+    "mnist_mislabeling_detection": files("quanda.benchmarks.resources.configs").joinpath(
+    "7b6c898-default_MislabelingDetection.yaml"),
+    "mnist_shortcut_detection": files("quanda.benchmarks.resources.configs").joinpath(
+    "7b6c898-default_ShortcutDetection.yaml"),
+    "mnist_class_detection": files("quanda.benchmarks.resources.configs").joinpath(
+    "7b6c898-default_ClassDetection.yaml"),
+    "mnist_model_randomization": files("quanda.benchmarks.resources.configs").joinpath(
+    "7b6c898-default_ClassDetection.yaml"),
 }
