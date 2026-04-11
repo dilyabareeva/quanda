6604c17aff2f8de8102312c40db730c8fa2c4992
Dilyara Bareeva <dilyabareeva@gmail.com>
2026-04-11 22:33:51 +0200

    chore: cifar bench train prep

--- scripts/cifar_resnet9_bench/bench_defs.sh
+++ scripts/cifar_resnet9_bench/bench_defs.sh
@@ -16,10 +16,10 @@ BENCH_PARAMS[MixedDatasets]="train_dataset=cifar10_train train_dataset.dataset_s
 BENCH_SWEEP[MixedDatasets]=""
 
 BENCH_PARAMS[ShortcutDetection]="train_dataset=cifar10_train_shortcut train_dataset.dataset_split='train' eval_dataset=cifar10_test_shortcut eval_dataset.dataset_split='test' +filter_by_shortcut_pred=true +filter_by_non_shortcut=true val_dataset=cifar10_val_shortcut"
-BENCH_SWEEP[ShortcutDetection]="model.trainer.lr=0.4,0.1,0.01 model.trainer.optimizer=adam,sgd model.trainer.max_epochs=12,24 train_dataset.wrapper.metadata.p=0.75,0.9,0.95 hydra.sweeper.n_trials=27"
+BENCH_SWEEP[ShortcutDetection]="model.trainer.max_epochs=12,24,36,48,96 train_dataset.wrapper.metadata.p=0.75,0.9,0.95 hydra.sweeper.n_trials=64"
 
 BENCH_PARAMS[MislabelingDetection]="train_dataset=cifar10_train_mislabeling train_dataset.dataset_split='train' eval_dataset=cifar10_test eval_dataset.dataset_split='test'"
-BENCH_SWEEP[MislabelingDetection]="model.trainer.lr=0.4,0.1,0.01 model.trainer.optimizer=adam,sgd model.trainer.max_epochs=12,24 train_dataset.wrapper.metadata.p=0.1,0.2 hydra.sweeper.n_trials=24"
+BENCH_SWEEP[MislabelingDetection]="model.trainer.max_epochs=12,24,36,48,96 train_dataset.wrapper.metadata.p=0.1,0.2 hydra.sweeper.n_trials=64"
 
 BENCH_PARAMS[LDS]="train_dataset=cifar10_train train_dataset.dataset_split='train' eval_dataset=cifar10_test eval_dataset.dataset_split='test'"
 BENCH_SWEEP[LDS]=""
