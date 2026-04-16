abfd2c45e3f4dffd34142c1748ff3652737595aa
Dilyara Bareeva <dilyabareeva@gmail.com>
2026-04-16 15:38:30 +0200

    fix: device in train and push to hub

--- scripts/train_and_push_to_hub.py
+++ scripts/train_and_push_to_hub.py
@@ -1,7 +1,6 @@
 from typing import Tuple
 
 import hydra
-import torch
 from omegaconf import DictConfig
 
 from quanda.benchmarks import bench_dict
@@ -17,13 +16,12 @@ from quanda.benchmarks.heuristics import *
     config_name="mnist_lenet",
 )
 def main(cfg: DictConfig) -> Tuple[float]:
-    device = "cuda" if torch.cuda.is_available() else "cpu"
     bench_cls = bench_dict[cfg.bench]
     logger = BenchConfigParser.parse_logger(cfg)
     bench_cls.train_and_push_to_hub(
         cfg,
         logger=logger,
-        device=device,
+        device=cfg.device,
     )
     return 0.0
 
