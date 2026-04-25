86dfd4e2d498319cb47ae4d0220cb85e06d428cd
Dilyara Bareeva <dilyabareeva@gmail.com>
2026-04-25 20:52:05 +0200

    chore: update eval scripts pt4

--- scripts/bert_qnli_bench/eval_qnli.sh
+++ scripts/bert_qnli_bench/eval_qnli.sh
@@ -5,17 +5,17 @@ source "$(dirname "$0")/eval_defs.sh"
 EVAL_CONFIG_NAME="bert_qnli"
 
 benchmarks=(
-    #qnli_class_detection
-    #qnli_mislabeling_detection
+    qnli_class_detection
+    qnli_mislabeling_detection
     qnli_mixed_datasets
-    #qnli_top_k_cardinality
-    #qnli_model_randomization
-    #qnli_linear_datamodeling
+    qnli_top_k_cardinality
+    qnli_model_randomization
+    qnli_linear_datamodeling
 )
 
 methods=(
-    #similarity
-    #kronfluence
+    similarity
+    kronfluence
     trak
     random
     representer_points
